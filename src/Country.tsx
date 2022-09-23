a5fa77689554e787cf7ff94ccab1cc6c9116e7e3
Cyfroni <karolplg@gmail.com>
2022-09-23 23:04:31 +0200

    simple links, details

--- src/Country.tsx
+++ src/Country.tsx
@@ -1,25 +1,54 @@
 import React from "react";
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 import { Country as Countrytype } from "./Countries";
 
 export const countryLoader = async ({ params }) => {
-  const response = await fetch(
+  let response = await fetch(
     `https://restcountries.com/v3.1/name/${params.countryName}`
   );
-  const data = await response.json();
+  if (!response.ok) {
+    response = await fetch(
+      `https://restcountries.com/v3.1/alpha/${params.countryName}`
+    );
+  }
 
-  //   const countries = data.map((val: any) => ({
-  //     name: val.name.common,
-  //     population: val.population,
-  //     region: val.region,
-  //     capital: val.capital,
-  //     flag: val.flags.svg,
-  //   }));
-  return { country: data };
+  const data = (await response.json())[0];
+  const country = {
+    name: data.name.common,
+    officialName: data.name.official,
+    population: data.population,
+    region: data.region,
+    subregion: data.subregion,
+    capital: data.capital,
+    flag: data.flags.svg,
+    tld: data.tld,
+    currencies: Object.values(data.currencies).map((c) => c.name),
+    languages: Object.values(data.languages),
+    neighbours: data.borders,
+  };
+  return { country };
 };
 
 export default function Country() {
-  const country = (useLoaderData() as any).country[0];
-  console.log(country);
-  return <div>{country.name.common}</div>;
+  const country = (useLoaderData() as any).country;
+  return (
+    <div>
+      <h2>{country.name}</h2>
+      <p>{country.officialName}</p>
+      <p>{country.population}</p>
+      <p>{country.region}</p>
+      <p>{country.subregion}</p>
+      <p>{country.capital}</p>
+      <p>{country.tld}</p>
+      <p>{country.currencies}</p>
+      <p>{country.languages}</p>
+      <ul>
+        {country.neighbours.map((n) => (
+          <li key={n}>
+            <Link to={`/countries/${n}`}>{n}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
--- src/CountryTile.tsx
+++ src/CountryTile.tsx
@@ -1,27 +1,30 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Country } from "./App";
 
 export default function CountryTile({ country }: { country: Country }) {
   return (
-    <article className="country">
-      <div className="country__flag">
-        <img src={country.flag} alt="" />
-      </div>
-      <div className="country__details">
-        <h2>{country.name}</h2>
-        <p>
-          <span>Population: </span>
-          {country.population}
-        </p>
-        <p>
-          <span>Region: </span>
-          {country.region}
-        </p>
-        <p>
-          <span>Capital: </span>
-          {country.capital}
-        </p>
-      </div>
-    </article>
+    <Link to={`countries/${country.name}`}>
+      <article className="country">
+        <div className="country__flag">
+          <img src={country.flag} alt="" />
+        </div>
+        <div className="country__details">
+          <h2>{country.name}</h2>
+          <p>
+            <span>Population: </span>
+            {country.population}
+          </p>
+          <p>
+            <span>Region: </span>
+            {country.region}
+          </p>
+          <p>
+            <span>Capital: </span>
+            {country.capital}
+          </p>
+        </div>
+      </article>
+    </Link>
   );
 }
